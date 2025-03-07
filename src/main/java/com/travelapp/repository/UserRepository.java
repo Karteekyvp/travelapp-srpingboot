cc1a48ba48113f94afc2028cfe1d783253fbcdab
Karteekyvp <yvpkarteek@gmail.com>
2025-03-06 20:12:23 -0500

    2nd

---
+++ src/main/java/com/travelapp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.travelapp.repository;
+
+import com.travelapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
+
