045754e0220425a380484f39bc41d42bf6035a3e
meghana-boda0611 <meghana.boda@gmail.com>
2025-03-06 19:56:48 -0500

    Initial Commit to backend

---
+++ src/test/java/com/travelapp/travelapp_backend/TravelappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelapp.travelapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
